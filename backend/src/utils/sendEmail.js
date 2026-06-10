cb4bb3817483a061e139e5a4ec0a3bec6b0e36f0
Vishal-web-ai <vishalmandal27503@gmail.com>
2026-06-10 20:57:45 +0530

    Switch to SendGrid

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,31 +1,12 @@
-const nodemailer = require('nodemailer')
-const dns = require('dns')
-
-const { promisify } = require('util')
-const resolve4 = promisify(dns.resolve4)
+const sgMail = require('@sendgrid/mail')
 
 async function sendEmail(to, subject, text) {
-    const addresses = await resolve4('smtp.gmail.com')
-    const smtpHost = addresses[0]
-
-    const transporter = nodemailer.createTransport({
-        host: smtpHost,
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        },
-        requireTLS: true,
-        connectionTimeout: 10000,
-        greetingTimeout: 10000
-    })
-
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: to,
-        subject: subject,
-        text: text
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    await sgMail.send({
+        to,
+        from: process.env.FROM_EMAIL,
+        subject,
+        text
     })
 }
 
